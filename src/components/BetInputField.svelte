9627acda47da332c04978260bfd06d9e2f2d37b1
erictakman <68903571+erictakman@users.noreply.github.com>
2022-10-03 18:38:25 +0200

    Fixed validation for number on input field

--- src/components/BetInputField.svelte
+++ src/components/BetInputField.svelte
@@ -1,40 +1,58 @@
 <script>
+	import { to_number } from "svelte/internal";
+
 	let inputValue = '';
-	let inputValueIsANumber = true;
 	let displayValue = '';
-	function handleOnSubmit() {
-		displayValue = inputValue;
+	let inputIsValid = true;
+	const handleOnSubmit = () => {
+		if (inputIsValid) {
+			displayValue = inputValue;
+		}
+	}
+	const handleOnChange = () => {
+		if (!to_number(inputValue)) {
+			inputIsValid = false;
+			console.log("change")
+			if (inputValue === '') inputIsValid = true
+		} else {
+			inputIsValid = true
+		}
 	}
 </script>
 
-<div class="flex gap-2">
-	<div class="flex items-baseline pt-1">
-		<p>
-			I would like to bet
-			<input
-				class={'border-b-2 rounded-lg p-1 px-2 m-1 focus:outline-none active:outline-none text-amber-500 font-bold w-36' +
-					(inputValueIsANumber ? ' ' : ' border-red-500')}
-				bind:value={inputValue}
-				id="inputField"
-				type="text"
-			/>
-			coins,
-		</p>
+<div class="flex flex-col justify-start gap-5">
+
+	<div class="flex gap-2">
+		<div class="flex items-baseline pt-1">
+			<p>
+				I would like to bet
+				<input
+					class={'border-b-2 rounded-lg p-1 px-2 m-1 focus:outline-none active:outline-none text-amber-500 font-bold w-36' + (!inputIsValid ? ' border-2 border-red-500' : '')}
+					bind:value={inputValue}
+					on:keyup={handleOnChange}
+					id="inputField"
+					type="text"
+				/>
+				coins,
+			</p>
+		</div>
+		<button
+			class="bg-gray-200 rounded-lg px-3 py-2 border-b-4 self-baseline border-gray-500 active:border-b-0 active:border-t-2"
+			on:click={handleOnSubmit}
+		>
+			let's go!
+		</button>
 	</div>
-	<button
-		class="bg-gray-200 rounded-lg px-3 py-2 border-b-4 self-baseline border-gray-500 active:border-b-0 active:border-t-2"
-		on:click={handleOnSubmit}
-	>
-		let's go!
-	</button>
+	
+	{#if displayValue != ''}
+		<p>
+			You have just betted
+			{displayValue}
+			coins 💰
+		</p>
+	{:else}
+		<p />
+	{/if}
+
 </div>
 
-{#if displayValue != ''}
-	<p>
-		You have just betted
-		{displayValue}
-		coins
-	</p>
-{:else}
-	<p />
-{/if}
